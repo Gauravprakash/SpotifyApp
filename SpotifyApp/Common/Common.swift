e92bd13407f3f1630d6af50b69481bc7edd68625
Gauravprakash <gaurav.prakashcool72@gmail.com>
2023-10-28 11:44:42 +0530

    Update Common.swift

--- SpotifyApp/Common/Common.swift
+++ SpotifyApp/Common/Common.swift
@@ -1,18 +1,11 @@
 //
 //  Common.swift
-//  Upkeep iOS Interview Project
 //
 //  Created by Gaurav Prakash | Upkeep on 10/24/23.
 //
 
 
 import Foundation
-
-struct Constants {
-    static let adminLoginButton = "Login as Admin"
-    static let userViewButton = "View Only User"
-    static let cacheKey = "cacheData"
-}
 
 struct ApiEndpoints
 {
